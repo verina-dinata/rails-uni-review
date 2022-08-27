d9018a904fc66f0a6d2abb106184bf54b83ec93c
Kavitha KC <kavithaprajod@gmail.com>
2022-08-27 17:23:20 +0800

    Sign-up css

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :password, confirmation: true
 end
