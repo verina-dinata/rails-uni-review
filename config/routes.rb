add12ca14dd8e90258cc3e685fa2211dd8f92f35
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-29 10:55:37 +0800

    Add users controller (#5)
    * Reseed file
    
    * Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :users do
+    get "users/profile", to: "users#profile", as: :profile
+  end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "education", to: "educations#index"
+
 end
