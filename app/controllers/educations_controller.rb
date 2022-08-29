9ae992d5ff20af93a960c97a38a986c4af656370
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-30 00:16:08 +0800

    Add form to modal

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -2,4 +2,13 @@ class EducationsController < ApplicationController
   def index
     @educations = Education.all
   end
+
+  def new
+    @education = Education.new
+    @universities = University.all
+  end
+
+  def create
+
+  end
 end
