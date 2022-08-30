f8fc1c08251fe712ccb75a595c4b99aebb89b2f3
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-30 20:05:33 +0800

    Add Education edit modal (#11)
    * Add education index page
    
    * Add education edit modal

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,6 +1,10 @@
 class EducationsController < ApplicationController
+  before_action :set_education, only: %i[ :edit update]
   def index
-    @educations = Education.all
+    @educations = current_user.educations
+    @education_new = Education.new
+    @universities = University.all
+    # @education_edit =
   end
 
   def new
@@ -18,9 +22,23 @@ class EducationsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @education.update(education_params)
+      redirect_to edit_education_path(@education), notice: "education has been updated."
+    else
+      render :edit
+    end
+  end
+
   private
 
   def education_params
     params.require(:education).permit(:course, :university_id, :department_id, :academic_degree, :university_email, :start_date, :end_date)
+  end
+
+  def set_education
+    @education = Education.find(params[:id])
   end
 end
