9e697973b0ec6092ff4bc19b0fa948bc4c497ea9
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-31 18:53:03 +0800

    WIP

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,5 +1,5 @@
 class UniversitiesController < ApplicationController
-  # skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!
 
   def index
     # @universities = University.all
@@ -16,6 +16,7 @@ class UniversitiesController < ApplicationController
   def show
     set_university
     @reviews = @university.reviews
+    # @education = Education
   end
 
   private
