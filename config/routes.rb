21a00cc1900bb2fd8b3de99897ce09596149232d
yamunaa kasavan <yamunaa94@gmail.com>
2022-09-04 11:38:06 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :educations
+  resources :favorites, only: [:create]
   # Defines the root path route ("/")
   # root "articles#index"
   # get "education", to: "educations#index"
