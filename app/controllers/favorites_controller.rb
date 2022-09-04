934f7ea84f0b3303e9a47d67fdf5888fd1b5aedc
yamunaa kasavan <yamunaa94@gmail.com>
2022-09-04 11:31:53 +0800

    completed controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,26 @@
 class FavoritesController < ApplicationController
+  before_action :set_favorite, only: %i[destroy]
+
+  def index
+    @favorites = policy_scope(Favorite)
+  end
+
+  def create
+    @university = University.find(params[:university_id])
+    @favorite = @university.favorites.build(user_id: current_user.id)
+    authorize @favorite
+    @favorite.save
+  end
+
+  def destroy
+    @university = @favorite.university
+    @favorite.destroy
+  end
+
+  private
+
+  def set_favorite
+    @favorite = Favorite.find(params[:id])
+    authorize @favorite
+  end
 end
