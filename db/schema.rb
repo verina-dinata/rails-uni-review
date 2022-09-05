a6bbcb9a5082e3e40137912c9112ad16e22f0b47
yamunaa kasavan <yamunaa94@gmail.com>
2022-09-06 01:16:50 +0800

    backend for fav works

--- db/schema.rb
+++ db/schema.rb
@@ -66,21 +66,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_084443) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.string "favoritable_type", null: false
-    t.bigint "favoritable_id", null: false
-    t.string "favoritor_type", null: false
-    t.bigint "favoritor_id", null: false
-    t.string "scope", default: "favorite", null: false
-    t.boolean "blocked", default: false, null: false
+    t.bigint "user_id", null: false
+    t.bigint "university_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["blocked"], name: "index_favorites_on_blocked"
-    t.index ["favoritable_id", "favoritable_type"], name: "fk_favoritables"
-    t.index ["favoritable_type", "favoritable_id", "favoritor_type", "favoritor_id", "scope"], name: "uniq_favorites__and_favoritables", unique: true
-    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable"
-    t.index ["favoritor_id", "favoritor_type"], name: "fk_favorites"
-    t.index ["favoritor_type", "favoritor_id"], name: "index_favorites_on_favoritor"
-    t.index ["scope"], name: "index_favorites_on_scope"
+    t.index ["university_id"], name: "index_favorites_on_university_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -157,6 +148,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_084443) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "educations", "universities"
   add_foreign_key "educations", "users"
+  add_foreign_key "favorites", "universities"
+  add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "universities"
   add_foreign_key "reviews", "users"
   add_foreign_key "votes", "reviews"
