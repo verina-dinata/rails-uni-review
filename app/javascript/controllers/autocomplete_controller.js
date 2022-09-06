0a2886edc3bad7b3e8a5991bfbf56104a509ab64
Kavitha KC <kavithaprajod@gmail.com>
2022-09-06 11:31:38 +0800

    Clean-up the autocomplete stimulus

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -27,7 +27,6 @@ export default class extends Controller {
   }
 
   select(event){
-    console.log(event.currentTarget.innerHTML)
     this.inputTarget.value = event.currentTarget.innerHTML
   }
 }
