2005f24d7d9580b8accc8ca2ee2e4ec7fea86781
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-08 00:06:08 +0800

    Remove debugger from registration controller (#37)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,10 +20,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  def update
-    debugger
-    super
-  end
+  # def update
+  #   super
+  # end
 
   # DELETE /resource
   # def destroy
