1a9dffe5cc0b23b9932de84ea7dff1a98362f7cc
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-09 11:19:08 +0800

    Email confirmation (#40)
    * wip
    
    * generate devise:views users
    
    * wip
    
    * Add css to user/sign_in page
    
    * Add more css to login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   validates :first_name, :last_name, presence: true
+
+  protected
+
+  def confirmation_required?
+    true
+  end
 end
