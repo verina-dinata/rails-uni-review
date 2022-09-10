7d4d4f222d590a7b0bba790b77a0038547dcce25
Yamunaa K <yamunaa94@gmail.com>
2022-09-10 13:39:08 +0800

    Favpage2 (#28)
    * added favorite controller, views, routes
    
    * update controller, show and routes
    
    * backend for fav works
    
    * changes made to fav controller
    
    * working on front-end
    
    * working on front-end
    
    * changed front-end
    
    * added path
    
    * amended
    
    * made changes to front-end

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :educations
+  resources :favorites, only: %i[index]
+
+  resources :universities do
+    resources :favorites, only: %i[create destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
   # get "education", to: "educations#index"
