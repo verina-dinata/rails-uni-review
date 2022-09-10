c04e427896f75ef8aa7e50754f7079650dbcfa06
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-10 18:15:27 +0800

    resolve conflict

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,6 +1,6 @@
 // import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="navbar"
+// // Connects to data-controller="navbar"
 // export default class extends Controller {
 //   connect() {
 //     console.log("hello navbar")
