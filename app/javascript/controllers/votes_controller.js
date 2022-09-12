9d3eb380608fd134e262deb316ee6469c644d116
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-12 20:23:51 +0800

    Add stimulus

--- app/javascript/controllers/votes_controller.js
+++ app/javascript/controllers/votes_controller.js
@@ -2,6 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="votes"
 export default class extends Controller {
+  static targets = ["like"]
   connect() {
+
+  }
+
+  upvote(event) {
+    event.preventDefault()
+    console.log("helllooo")
+    console.log(this.element)
+    console.log(this.likeTarget)
   }
 }
