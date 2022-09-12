a54900af8955febeb397350b0b8531a4f1e09a5b
Kavitha KC <kavithaprajod@gmail.com>
2022-09-12 21:12:17 +0800

    Animation to banner

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -33,3 +33,6 @@ application.register("navbar", NavbarController)
 
 import StarRatingController from "./star_rating_controller"
 application.register("star-rating", StarRatingController)
+
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
