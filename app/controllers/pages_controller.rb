44af11dd2ca5b861818ee31c79eab40545d26136
Kavitha KC <kavithaprajod@gmail.com>
2022-09-13 18:46:29 +0800

    Add carousel to review

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,9 @@ class PagesController < ApplicationController
     @first_review = Review.first
     @third_review = Review.third
     @last_review = Review.last
+    @second_review = Review.second
+    @fourth_review = Review.fourth
+    @fifth_review = Review.fifth
   end
 
   def home_search
@@ -19,15 +22,4 @@ class PagesController < ApplicationController
     end
   end
 
-  def home_search
-    @universities = []
-    if params[:query].present?
-      sql_query = "name ILIKE :query OR country ILIKE :query OR city ILIKE :query"
-      @universities = University.where(sql_query, query: "%#{params[:query]}%")
-    end
-    respond_to do |format|
-      # format.html # { redirect_to root_path }   Follow regular flow of Rails
-      format.text { render partial: "universities/list", locals: { universities: @universities }, formats: [:html] }
-    end
-  end
 end
