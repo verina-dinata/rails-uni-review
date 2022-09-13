299e1ba999dd2f566f61b6ee95463c624eb455af
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-13 12:03:55 +0800

    Add javascript

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,12 @@ class VotesController < ApplicationController
   before_action :set_review, only: %i[create]
 
   def create
-    @vote = Vote.new(vote_params)
+    puts params
+    @vote = Vote.new(status: params[:vote])
     @vote.user = current_user
     @vote.review = @review
     @vote.save
+
   end
 
   private
@@ -14,8 +16,5 @@ class VotesController < ApplicationController
     @review = Review.find(params[:review_id])
   end
 
-  def vote_params
-    params.require(:vote).permit(:status)
-  end
 
 end
--- app/javascript/controllers/votes_controller.js
+++ app/javascript/controllers/votes_controller.js
@@ -2,15 +2,68 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="votes"
 export default class extends Controller {
-  static targets = ["like"]
+  static targets = ["totalLike", "upvoteIcon", "upvoteButton"]
+  static values = { universityId: Number, reviewId: Number}
   connect() {
 
   }
 
   upvote(event) {
+    console.log("testttt")
     event.preventDefault()
-    console.log("helllooo")
-    console.log(this.element)
-    console.log(this.likeTarget)
+
+    const url = `/universities/${this.universityIdValue}/reviews/${this.reviewIdValue}/votes`
+
+    let csrfToken = document
+                .querySelector('meta[name="csrf-token"]')
+                .getAttribute("content")
+    fetch(url, {
+      method: "POST",
+      headers: {"Accept": "application/json",
+      "Content-Type": "application/json",
+      "X-CSRF-Token": csrfToken},
+      body: JSON.stringify({"vote": "upvote"})
+    })
+      .then(response =>  {if (response.ok){
+        // console.log("success")
+
+        // this.upvoteIconTarget.classList.remove("unvoted")
+        // this.upvoteIconTarget.classList.add("upvoted")
+        if (this.upvoteIconTarget.vote) {
+          this.totalLikeTarget.innerText = parseInt(this.totalLikeTarget.innerText) + 1
+          this.upvoteIconTarget.classList.remove("upvoted")
+          this.upvoteIconTarget.classList.add("unvoted")
+        } else {
+          this.totalLikeTarget.innerText = parseInt(this.totalLikeTarget.innerText) - 1
+          this.upvoteIconTarget.classList.remove("unvoted")
+          this.upvoteIconTarget.classList.add("upvoted")
+
+        }
+        // this.upvoteButtonTarget.disabled = true
+      } else {
+        console.log("fails")
+      }
+
+      })
+
+      // .then((data) => {
+      //   // console.log(data)
+      // })
+    // // console.log(url)
+    // const likeNumber = this.totalLikeTarget
+    // // this.totalLikeTarget.innerText += 1
+    // // console.log(likeNumber.innerText)
+    // let a = likeNumber.innerText
+    // // parseInt(a) += 1
+    // console.log(a)
+    // // console.log(this.upvoteIconTarget)
+
+
+    // If user already upvotes & clicks upvote again,
+      // upvote icon shouldn't be clicked (undo upvote) & upvote total decrease by 1
+    // else
+      // upvote icon turns green * upvote total increase by 1
   }
+
+
 }
