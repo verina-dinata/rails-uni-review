2bd84417b9790b196f3e926f8843e9b70fbf129f
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-14 14:11:12 +0800

    Add messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,7 @@ Rails.application.routes.draw do
 
   resources :reviews, only: :destroy
 
-  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 end
