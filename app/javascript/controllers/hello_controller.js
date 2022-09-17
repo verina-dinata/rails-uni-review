79b9f7ee122c0850d5b26303c0f3792b92b6a1c1
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-17 14:24:06 +0800

    Add tooltip on review button wip

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,7 @@
-import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
-  }
-}
+import "bootstrap"
+
+var exampleEl = document.getElementById('btn-tooltip')
+var tooltip = new bootstrap.Tooltip(exampleEl, {
+  boundary: document.body // or document.querySelector('#boundary')
+})
