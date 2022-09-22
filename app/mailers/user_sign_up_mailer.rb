40dd5d6c4ac16404bc18ab5eb7fb2ecabb45bcfe
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-22 16:37:06 +0800

    add sendgrid smtp setting

--- app/mailers/user_sign_up_mailer.rb
+++ app/mailers/user_sign_up_mailer.rb
@@ -1,2 +1,10 @@
 class UserSignUpMailer < ApplicationMailer
+  default :from => 'support@u-review.co'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Confirm Your U-Review Account Email Address' )
+  end
 end
