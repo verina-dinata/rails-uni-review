86aa21c813a305146b2243347347d3961be90e23
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-22 17:22:12 +0800

    send email for user sign up in development

--- app/mailers/user_sign_up_mailer.rb
+++ app/mailers/user_sign_up_mailer.rb
@@ -1,10 +1,13 @@
 class UserSignUpMailer < ApplicationMailer
-  default :from => 'support@u-review.co'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
-    mail( :to => @user.email,
-    :subject => 'Confirm Your U-Review Account Email Address' )
+    raise
+    mail(
+      to: @user.email,
+      subject: 'Confirm Your U-Review Account Email Address',
+      from: 'support@u-review.co'
+    )
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'admin@uni-review.com'
+  config.mailer_sender = 'support@u-review.co'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
