10be700c198490537ed1981f0132394fa71625c1
peekachoo <jonathan.taysc@gmail.com>
2022-09-24 08:46:38 +0800

    Revise Show 24 Sep

--- app/models/university.rb
+++ app/models/university.rb
@@ -3,6 +3,8 @@ class University < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites
   has_many :educations
+  has_many :courses
+
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
