5be3fb57dcb5bbfa2bd760ed5dbb0adada59307a
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-26 23:20:58 +0800

    add local timezone

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module RailsUniReview
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Asia/Singapore"
   end
 end
